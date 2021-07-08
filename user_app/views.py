cfcf4b05174450d38578ae86760bd1e4a64d3510
moelogins <moelogins@gmail.com>
2021-07-08 16:27:31 -0400

    created the 'indexView' and 'coach_details' views along with their urls. Also named index.html and coach_details.html templates.

--- user_app/views.py
+++ user_app/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 from .forms import CreateHeroForm
 from .models import HeroUser
-
-
-
-
-
+from tasks.models import Task
 
 # more to do here, just created the view to match the form/model, error catching needs to be worked on as well
 def createUser(request):
@@ -33,3 +29,22 @@ def createUser(request):
         return render(request, '????????.html', {'form': form})
   form = CreateHeroForm()
   return render(request, '?????????.html', {'form': form})
+
+
+def indexView(request):
+    welcome = 'Welcome to iHero!'
+    context = {
+        'welcome': welcome,
+    }
+    return render(request, 'index.html', context)
+
+
+def coach_details(request, user_id: int):
+    if HeroUser.objects.filter(is_coach=True):
+        coach = HeroUser.objects.get(id=user_id)
+        assigned_tasks = Task.objects.filter(assigned_to=coach)
+        context = {
+            'coaches': coach,
+            'assigned_tasks': assigned_tasks,
+        }
+        return render(request, 'coach_details.html', context)
