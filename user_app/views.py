4a69b6ae8d146fd79e6bcda77354f626d47fb211
Rochelle Edwards <rochellelynn.programmer@gmail.com>
2021-07-09 12:10:40 -0500

    updated create user view and added login view

--- user_app/views.py
+++ user_app/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
-from .forms import CreateHeroForm
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from .forms import CreateHeroForm, LoginHeroForm
 from .models import HeroUser
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 
 
 
 
+def loginUser(request):
+  if request.method == "POST":
+    form = LoginHeroForm(request.POST)
+    if form.is_valid():
+      data = form.cleaned_data
+      if HeroUser.objects.filter(username=data['username']).exists():
+        user = authenticate(username=data['username'], password=data['password'])
+        login(request, user)
+        return HttpResponseRedirect(request.GET.get('next', reverse('?????')))
+      else:
+        return render(request, 'genericform.html', {'form': form, 'title': 'Login Page', 'message': 'If you already have an account, please verify that you are using correct login credentials. If you do not have an account, please create one'})
+  form = LoginHeroForm()
+  return render(request, 'genericform.html', {'form': form, 'title': "Login Page", 'message': 'Please Login into your account'})
 
-# more to do here, just created the view to match the form/model, error catching needs to be worked on as well
+
 def createUser(request):
   if request.method == 'POST':
     form = CreateHeroForm(request.POST)
@@ -15,7 +30,7 @@ def createUser(request):
       data = form.cleaned_data
       if HeroUser.objects.filter(username=data['username']).exists()==False:
         if data['password1'] == data['password2']:
-          user = HeroUser.objects.create_user(
+          new_user = HeroUser.objects.create_user(
           username=data['username'],
           first_name=data['first_name'],
           last_name=data['last_name'],
@@ -24,12 +39,32 @@ def createUser(request):
           interests = data['interests'], 
           website = data['website'],
           bio = data['bio'],
-          age=data['age']
-        )
-        if data['is_coach'] == True:
-          user.is_staff = True
-          user.save()
-        
-        return render(request, '????????.html', {'form': form})
+          age=data['age'],
+          is_coach = data['is_coach']
+          )
+          if data['is_coach'] == True:
+            new_user.is_staff = True
+            new_user.save()
+          user = authenticate(username=data['username'], password=data['password1'])
+          login(request, user)
+          return HttpResponseRedirect(reverse('home'))
+        else:
+          return render(request, 'genericform.html', {'form': form, 'message': "Please make sure passwords match", 'title': "Create New Account"})
+      else:
+        return render(request, 'genericform.html', {'form': form, 'title': "Create New Account", 'message': 'Username is unavailable, please choose another.'})
   form = CreateHeroForm()
-  return render(request, '?????????.html', {'form': form})
+  return render(request, 'genericform.html', {'form': form, 'title': "Create New Account", 'message': "Please fill out this form to create your new account"})
+
+def loginUser(request):
+  if request.method == "POST":
+    form = LoginHeroForm(request.POST)
+    if form.is_valid():
+      data = form.cleaned_data
+      if HeroUser.objects.filter(username=data['username']).exists():
+        user = authenticate(username=data['username'], password=data['password'])
+        login(request, user)
+        return HttpResponseRedirect(request.GET.get('next', reverse('home')))
+      else:
+        return render(request, 'genericform.html', {'form': form, 'title': 'Login Page', 'message': 'If you already have an account, please verify that you are using correct login credentials. If you do not have an account, please create one'})
+  form = LoginHeroForm()
+  return render(request, 'genericform.html', {'form': form, 'title': "Login Page", 'message': 'Please Login into your account'})
