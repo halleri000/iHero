d9729c58eababe3e5de91b8e06e202eb0430409d
Rochelle Edwards <rochellelynn.programmer@gmail.com>
2021-07-13 15:07:37 -0500

    updated model in attempt to feed front end a string instead of a code, added dependency, hide secret

--- user_app/models.py
+++ user_app/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class HeroUser(AbstractUser):
-  SERVICEINDUSTRY= "SI"
-  TECH ='TK'
-  SALES = 'SL'
-  HEALTHCARE = 'HC'
-  FINANCE = 'FN'
-  EDUCATION = 'ED'
+  SERVICEINDUSTRY = 'Service Industry'
+  TECH ='Technology'
+  SALES = 'Sales'
+  HEALTHCARE = 'Health Care'
+  FINANCE = 'Finance'
+  EDUCATION = 'Education'
 
   INTEREST_CHOICES = [
     (SERVICEINDUSTRY, 'Service Industry'),
@@ -22,7 +22,7 @@ class HeroUser(AbstractUser):
 
   email= models.EmailField(max_length=254)
   age = models.IntegerField(default=18)
-  interests = models.CharField(choices = INTEREST_CHOICES, max_length=2, default=TECH)
+  interests = models.CharField(choices = INTEREST_CHOICES, max_length=20, default=TECH)
   bio = models.TextField()
   website = models.URLField(max_length=200)
   is_coach = models.BooleanField(default=False)
